2fce5cd0c334722a0677ca6b888ad8d99b05e19b
dghvici <daniela.ghinchevici@gmail.com>
2025-02-27 10:08:32 +0000

    fixed IAM for Lambda write to S3

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,3 +1,28 @@
+# Lambda IAM Role
+
+# Define
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+# Create
+resource "aws_iam_role" "iam_for_lambda" {
+  name               = "iam_for_lambda"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+# Lambda IAM Policy for S3 Write
+
+# Define
 data "aws_iam_policy_document" "s3_document" {
   statement {
     actions = [
@@ -13,44 +38,18 @@ data "aws_iam_policy_document" "s3_document" {
 }
 
 
+# Create
 resource "aws_iam_policy" "s3_policy" {
   name_prefix = "s3-policy-terrific-totes-lambda-"
   policy      = data.aws_iam_policy_document.s3_document.json
 }
 
-resource "aws_iam_role" "iam_for_lambda" {
-  name               = "iam_for_lambda"
-  assume_role_policy = data.aws_iam_policy_document.assume_role.json
-}
 
-data "aws_iam_policy_document" "assume_role" {
-  statement {
-    effect = "Allow"
-
-    principals {
-      type        = "Service"
-      identifiers = ["lambda.amazonaws.com"]
-    }
-
-    actions = ["sts:AssumeRole"]
-  }
-}
-
-# resource "aws_iam_role" "lambda_role" {
-#   name_prefix        = "role-lambdas-"
-#   assume_role_policy = aws_iam_policy_attachment.lambda_policy.json
-# }
-
-resource "aws_iam_policy_attachment" "lambda_policy" {
-  name = "lambda_policy_for_lambda"
-  policy_arn = data.aws_iam_policy_document.assume_role.json
-  roles = [aws_iam_role.iam_for_lambda.name]
-}
-
-resource "aws_iam_policy_attachment" "lambda_s3_attachment" {
-  name       = "s3_policy_for_lambda"
+# Attach
+resource "aws_iam_role_policy_attachment" "lambda_s3_attachment" {
+  role = aws_iam_role.iam_for_lambda.name
   policy_arn = aws_iam_policy.s3_policy.arn
-  roles = [aws_iam_role.iam_for_lambda.name]
 }
+
 
 
