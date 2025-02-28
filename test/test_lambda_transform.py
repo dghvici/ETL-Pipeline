0b545f876ebf6462f75e3818bcc51f036677dfc9
Callum-Bain <cbain_88@hotmail.com>
2025-02-28 10:19:24 +0000

    name change in test_lambda_transform

--- test/test_lambda_transform.py
+++ test/test_lambda_transform.py
@@ -1,8 +1,8 @@
-from src.lambda_transform import lambda_handler
+from src.lambda_transform import lambda_handler_transform
 
 
 def test_lambda_handler():
     # placeholder for the purpose of the CICD pipeline
     # replace with appropriate tests
-    result = lambda_handler()
+    result = lambda_handler_transform()
     assert result
