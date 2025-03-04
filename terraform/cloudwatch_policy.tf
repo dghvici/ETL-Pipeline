151602ffcdc4ffd4029127546916ee3c6cd55fa5
Ollie Lawn <ollielawn@ollies-mbp-4.home>
2025-03-04 12:16:56 +0000

    github secrets succesfully migrating to terraform

--- terraform/cloudwatch_policy.tf
+++ terraform/cloudwatch_policy.tf
@@ -1,57 +1,57 @@
-# # permissions for logging, metrics and alarms 
+# permissions for logging, metrics and alarms 
 
-# resource "aws_iam_policy" "cloudwatch_policy" {
-#     name = "CloudWatchPolicy"
-#     description = "Policy for CloudWatch logging, metrics and alarms"
-#     policy = jsonencode({
-#         Version = "2012-10-17"
-#         Statement = [
-#             {
-#                 Effect = "Allow"
-#                 Action = [
-#                     "logs:CreateLogGroup",
-#                     "logs:CreateLogStream",
-#                     "logs:PutLogEvents",
-#                     "logs:DescribeLogGroups",
-#                     "logs:DescribeLogStreams",
-#                     "logs:GetLogEvents",
-#                     "logs:FilterLogEvents"
-#                 ],
-#                 "Resource": "*" 
-#             },
-#             {
-#                 Effect = "Allow"
-#                 Action = [
-#                     "cloudwatch:PutMetricData",
-#                     "cloudwatch:GetMetricStatistics",
-#                     "cloudwatch:ListMetrics",
-#                     "cloudwatch:ListMetricStreams",
-#                     "cloudwatch:StartMetricStreams",
-#                     "cloudwatch:StopMetricStreams",
-#                     "cloudwatch:SetAlarmState",
-#                     "cloudwatch:PutMetricAlarm",
-#                     "cloudwatch:DescribeAlarms",
-#                     "cloudwatch:EnableAlarmActions",
-#                     "cloudwatch:DisableAlarmActions",
-#                     "cloudwatch:DeleteAlarms"
-#                 ],
-#                 "Resource": "*"
-#             },
-#             {
-#                 Effect = "Allow"
-#                 Action = [
-#                     "sns:Subscribe",
-#                     "sns:Publish"
-#                 ],
-#                 "Resource": "*"
-#             }
-#         ]
-#     }) 
-# }
-# resource "aws_iam_role_policy_attachment" "cloudwatch_policy" {
-#   role          = aws_iam_policy.cloudwatch_policy.name
-#   policy_arn    =  aws_iam_policy.cloudwatch_policy.arn
-# }
+resource "aws_iam_policy" "cloudwatch_policy" {
+    name = "CloudWatchPolicy"
+    description = "Policy for CloudWatch logging, metrics and alarms"
+    policy = jsonencode({
+        Version = "2012-10-17"
+        Statement = [
+            {
+                Effect = "Allow"
+                Action = [
+                    "logs:CreateLogGroup",
+                    "logs:CreateLogStream",
+                    "logs:PutLogEvents",
+                    "logs:DescribeLogGroups",
+                    "logs:DescribeLogStreams",
+                    "logs:GetLogEvents",
+                    "logs:FilterLogEvents"
+                ],
+                "Resource": "*" 
+            },
+            {
+                Effect = "Allow"
+                Action = [
+                    "cloudwatch:PutMetricData",
+                    "cloudwatch:GetMetricStatistics",
+                    "cloudwatch:ListMetrics",
+                    "cloudwatch:ListMetricStreams",
+                    "cloudwatch:StartMetricStreams",
+                    "cloudwatch:StopMetricStreams",
+                    "cloudwatch:SetAlarmState",
+                    "cloudwatch:PutMetricAlarm",
+                    "cloudwatch:DescribeAlarms",
+                    "cloudwatch:EnableAlarmActions",
+                    "cloudwatch:DisableAlarmActions",
+                    "cloudwatch:DeleteAlarms"
+                ],
+                "Resource": "*"
+            },
+            {
+                Effect = "Allow"
+                Action = [
+                    "sns:Subscribe",
+                    "sns:Publish"
+                ],
+                "Resource": "*"
+            }
+        ]
+    }) 
+}
+resource "aws_iam_role_policy_attachment" "cloudwatch_policy" {
+  role          = aws_iam_policy.cloudwatch_policy.name
+  policy_arn    =  aws_iam_policy.cloudwatch_policy.arn
+}
 
 
 
--- terraform/secrets_manager.tf
+++ terraform/secrets_manager.tf
@@ -1,5 +1,4 @@
 resource "aws_secretsmanager_secret" "rds_user" {
-  #checkov:skip=CKV2_AWS_57: This variable does not need to be rotated
   name                    = "rds_user"
   recovery_window_in_days = 0
   kms_key_id              = aws_kms_key.local_key.id
@@ -10,11 +9,9 @@ resource "aws_secretsmanager_secret_version" "rds_user_version" {
   secret_string = var.rds_user
 
   depends_on = [ aws_secretsmanager_secret.rds_password ]
-  #The value is passed to the Terraform via the CLI
 }
 
 resource "aws_secretsmanager_secret" "rds_password" {
-  #checkov:skip=CKV2_AWS_57: This variable does not need to be rotated
   name                    = "rds_password"
   recovery_window_in_days = 0
   kms_key_id              = aws_kms_key.local_key.id
@@ -23,11 +20,10 @@ resource "aws_secretsmanager_secret" "rds_password" {
 resource "aws_secretsmanager_secret_version" "rds_password_version" {
   secret_id     = aws_secretsmanager_secret.rds_password.id
   secret_string = var.rds_password
-  #The value is passed to the Terraform via the CLI
 }
 
 resource "aws_secretsmanager_secret" "rds_name" {
-  #checkov:skip=CKV2_AWS_57: This variable does not need to be rotated
+  
   name                    = "rds_name"
   recovery_window_in_days = 0
   kms_key_id              = aws_kms_key.local_key.id
@@ -36,12 +32,10 @@ resource "aws_secretsmanager_secret" "rds_name" {
 resource "aws_secretsmanager_secret_version" "rds_name_version" {
   secret_id     = aws_secretsmanager_secret.rds_name.id
   secret_string = var.rds_name
-  #The value is passed to the Terraform via the CLI
 }
 
 
 resource "aws_secretsmanager_secret" "rds_host" {
-  #checkov:skip=CKV2_AWS_57: This variable does not need to be rotated
   name                    = "rds_host"
   recovery_window_in_days = 0
   kms_key_id              = aws_kms_key.local_key.id
@@ -50,11 +44,9 @@ resource "aws_secretsmanager_secret" "rds_host" {
 resource "aws_secretsmanager_secret_version" "rds_host_version" {
   secret_id     = aws_secretsmanager_secret.rds_host.id
   secret_string = var.rds_host
-  #The value is passed to the Terraform via the CLI
 }
 
 resource "aws_secretsmanager_secret" "port" {
-  #checkov:skip=CKV2_AWS_57: This variable does not need to be rotated
   name                    = "port"
   recovery_window_in_days = 0
   kms_key_id              = aws_kms_key.local_key.id
@@ -63,5 +55,4 @@ resource "aws_secretsmanager_secret" "port" {
 resource "aws_secretsmanager_secret_version" "port_version" {
   secret_id     = aws_secretsmanager_secret.port.id
   secret_string = var.port
-  #The value is passed to the Terraform via the CLI
 }
