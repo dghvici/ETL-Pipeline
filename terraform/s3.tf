2db6738b912527f77051c8f96d756c2f8e870f48
Ollie Lawn <ollielawn@ollies-mbp-4.home>
2025-03-10 15:39:33 +0000

    testing eventbridge provisioning -WIP

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,5 +1,5 @@
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
-# c
+
 
 resource "aws_s3_bucket" "ingestion_bucket" {
   bucket  = "etl-lullymore-west-ingested"
