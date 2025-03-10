b4e168086df42899362feac9e473e9317d1f2edf
Lucy Milligan <lucyjmilligan@yahoo.co.uk>
2025-03-10 17:52:10 +0000

    update the formatting of the load files to pass flake-8

--- test_utils/test_transform_load_utils.py
+++ test_utils/test_transform_load_utils.py
@@ -1,11 +1,12 @@
 from utils.transform_load_utils import get_table_name
 import pytest
+
 # import boto3
 # from moto import mock_aws
-import os
+# import os
 
 
-class TestGetTableName():
+class TestGetTableName:
     def test_get_table_name_returns_transformed_table_name(self):
         file_key = "2025/3/transformed-sales-2025-03-05 15:00:22.634136"
         expected = "sales"
@@ -22,5 +23,3 @@ class TestGetTableName():
         with pytest.raises(UnboundLocalError):
             file_key = "test"
             get_table_name(file_key)
-
-            
--- utils/transform_load_utils.py
+++ utils/transform_load_utils.py
@@ -1,7 +1,8 @@
 import re
 import boto3
-import pyarrow.parquet as pq
-from io import BytesIO
+
+# import pyarrow.parquet as pq
+# from io import BytesIO
 import logging
 
 s3_client = boto3.client("s3")
@@ -9,9 +10,9 @@ s3_client = boto3.client("s3")
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
+
 def get_table_name(file_key):
-    """function to extract the table name from the
-    filenames"""
+    """Function to extract the table name from the filenames."""
     try:
         if "ingested" in file_key:
             re_pattern = r"ingested-(\w+)"
@@ -21,9 +22,9 @@ def get_table_name(file_key):
         match = re.search(re_pattern, file_key)
         table_name = match.group(1)
         return table_name
-    except UnboundLocalError as e:
-        logger.error("File name does not conform to the expected \
-        format (does not include 'ingested' or 'transformed')")
+    except UnboundLocalError:
+        logger.error(
+            "File name does not conform to the expected \
+        format (does not include 'ingested' or 'transformed')"
+        )
         raise
-
-
