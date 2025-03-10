ad2bc2c5151313344a342ca1cb9d9798b0beb63a
Ollie Lawn <ollielawn@ollies-mbp-4.home>
2025-03-10 15:20:34 +0000

    testing eventbridge provisioning -WIP

--- terraform/eventbridge.tf
+++ terraform/eventbridge.tf
@@ -45,38 +45,38 @@ resource "aws_iam_role_policy_attachment" "eventbridge_lambda_attach" {
 
 ############################### TRANSFORM ##################################
 
-#Rule for Ingestion Bucket
-resource "aws_cloudwatch_event_rule" "s3_object_created_transform" {
-  name        = "s3-eventbridge-rule-transform"
-  description = "Capture S3 object created events"
-  event_pattern = <<EOF
-{
-  "source": ["aws.s3"],
-  "detail-type": ["Object Created"],
-  "detail": {
-    "bucket": {
-      "name": ["etl-lullymore-west-ingested"]
-    }
-  }
-}
-EOF
-}
+# #Rule for Ingestion Bucket
+# resource "aws_cloudwatch_event_rule" "s3_object_created_transform" {
+#   name        = "s3-eventbridge-rule-transform"
+#   description = "Capture S3 object created events"
+#   event_pattern = <<EOF
+# {
+#   "source": ["aws.s3"],
+#   "detail-type": ["Object Created"],
+#   "detail": {
+#     "bucket": {
+#       "name": ["etl-lullymore-west-ingested"]
+#     }
+#   }
+# }
+# EOF
+# }
 
-#Lambda Target > Transform
-resource "aws_cloudwatch_event_target" "lambda_target_transform" {
-  rule      = aws_cloudwatch_event_rule.s3_object_created_transform.name
-  target_id = "SendToLambda"
-  arn       = aws_lambda_function.transform_function.arn
-}
+# #Lambda Target > Transform
+# resource "aws_cloudwatch_event_target" "lambda_target_transform" {
+#   rule      = aws_cloudwatch_event_rule.s3_object_created_transform.name
+#   target_id = "SendToLambda"
+#   arn       = aws_lambda_function.transform_function.arn
+# }
 
 
-resource "aws_lambda_permission" "allow_eventbridge_transform" {
-  statement_id  = "AllowExecutionFromEventBridge"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.transform_function.function_name
-  principal     = "events.amazonaws.com"
-  source_arn    = aws_cloudwatch_event_rule.s3_object_created_transform.arn
-}
+# resource "aws_lambda_permission" "allow_eventbridge_transform" {
+#   statement_id  = "AllowExecutionFromEventBridge"
+#   action        = "lambda:InvokeFunction"
+#   function_name = aws_lambda_function.transform_function.function_name
+#   principal     = "events.amazonaws.com"
+#   source_arn    = aws_cloudwatch_event_rule.s3_object_created_transform.arn
+# }
 
 ############################## LOAD ######################################
 
@@ -159,4 +159,4 @@ resource "aws_s3_bucket_notification" "bucket_notification_transform" {
   }
 
   depends_on = [aws_lambda_permission.allow_bucket_transform]
-} 
+}
