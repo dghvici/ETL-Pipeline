28924dee0590e9cca0c76b11ec98581e4635bd08
Callum-Bain <cbain_88@hotmail.com>
2025-03-10 11:56:52 +0000

    dataframe creating working but now moving that functionality to a util function

--- src/lambda_transform.py
+++ src/lambda_transform.py
@@ -4,14 +4,26 @@ import json
 import boto3
 from datetime import datetime as dt
 import io
+from pprint import pprint
+
+
+def get_currency(currency):
+    if currency == "GBP":
+        return "pound"
+    elif currency == "EUR":
+        return "euro"
+    elif currency == "USD":
+        return "dollar"
+    else:
+        return "other"
 
 
 def lambda_handler_transform(event, context):
-    s3 = boto3.client('s3')
-    transform_bucket = "etl-lullymore-west-transformed"
+    # s3 = boto3.client('s3')
+    # transform_bucket = "etl-lullymore-west-transformed"
 
-    source_bucket = event['Records'][0]['s3']['bucket']['name']
-    source_key = event['Records'][0]['s3']['object']['key']
+    # source_bucket = event['Records'][0]['s3']['bucket']['name']
+    # source_key = event['Records'][0]['s3']['object']['key']
 
     # Trigger an S3 event (JSON file uploaded)- this will be the event
     # Get the object from the event and show its content type
@@ -20,41 +32,35 @@ def lambda_handler_transform(event, context):
 # Load json data from bucket
     # try:
 
-    response = s3.get_object(Bucket=source_bucket, Key=source_key)
-    file_content = response['Body'].read().decode('utf-8')
+    # response = s3.get_object(Bucket=source_bucket, Key=source_key)
+    # file_content = response['Body'].read().decode('utf-8')
 
-    data = json.loads(file_content)
+    with open('src/data.json', 'r') as file:
+        data = json.load(file)
 
-    # for table_name in data['New_data']:
-    #     df_tables[table_name] = \
-    #         pd.DataFrame(data[table_name]["rows"],
-    #                      columns=data[table_name]["column_names"])
-        
-
-    for table_dict in data['New_data']: 
-        for table_name, table_data in table_dict.items():  
+    for table_dict in data['New_data']:
+        for table_name, table_data in table_dict.items():
             df_tables[table_name] = pd.DataFrame(
                 table_data["rows"], columns=table_data["column_names"]
-        )
+            )
 
     final_dataframes = {}
+    pprint(df_tables)
 
     if "sales_order" in df_tables:
         df_sales_order = df_tables["sales_order"]
 
         # Convert date columns
         df_sales_order['created_at'] = pd.to_datetime(
-            df_sales_order['created_at'])
+            df_sales_order['created_at'], format='ISO8601')
         df_sales_order['last_updated'] = pd.to_datetime(
-            df_sales_order['last_updated'])
+            df_sales_order['last_updated'], format='ISO8601')
         df_sales_order['agreed_payment_date'] = pd.to_datetime(
-            df_sales_order['agreed_payment_date'])
+            df_sales_order['agreed_payment_date'], format='ISO8601')
         df_sales_order['agreed_delivery_date'] = pd.to_datetime(
-            df_sales_order['agreed_delivery_date'])
+            df_sales_order['agreed_delivery_date'], format='ISO8601')
 
         df_fact_sales_order = pd.DataFrame()
-        df_fact_sales_order['sales_records_id'] \
-            = range(1, len(df_fact_sales_order) + 1)
         df_fact_sales_order['sales_order_id'] \
             = df_sales_order['sales_order_id']
         df_fact_sales_order['created_date'] \
@@ -83,17 +89,29 @@ def lambda_handler_transform(event, context):
             = df_sales_order['agreed_delivery_date']
         df_fact_sales_order['agreed_delivery_location_id'] \
             = df_sales_order['agreed_delivery_location_id']
+        # sales_record_id needs to be stored externally e.g. parameter store.
+        # so the number can continue on each lambda invocation.
+        df_fact_sales_order['sales_records_id'] \
+            = range(1, len(df_fact_sales_order) + 1)
+        df_fact_sales_order = df_fact_sales_order[[
+                              'sales_records_id', 'sales_order_id',
+                              'created_date', 'created_time',
+                              'last_updated_date', 'last_updated_time',
+                              'sales_staff_id', 'counterparty_id',
+                              'units_sold', 'unit_price',
+                              'currency_id', 'design_id',
+                              'agreed_payment_date',
+                              'agreed_delivery_date',
+                              'agreed_delivery_location_id']]
         final_dataframes["df_fact_sales_order"] \
             = df_fact_sales_order
 
-    if "staff" in df_tables and "department" in df_tables and \
-            "sales_order" in df_tables:
+    if "staff" in df_tables and "department" in df_tables:
         df_staff = df_tables["staff"]
         df_department = df_tables["department"]
-        df_sales_order = df_tables["sales_order"]
 
         df_dim_staff = pd.DataFrame({
-            "staff_id": df_sales_order['staff_id'],
+            "staff_id": df_staff['staff_id'],
             "first_name": df_staff['first_name'],
             "last_name": df_staff['last_name'],
             "department_name": df_department['department_name'],
@@ -102,13 +120,12 @@ def lambda_handler_transform(event, context):
         })
         final_dataframes["df_dim_staff"] = df_dim_staff
 
-    if "address" in df_tables and "sales_order" in df_tables:
+    if "address" in df_tables:
         df_address = df_tables["address"]
-        df_sales_order = df_tables["sales_order"]
 
         df_dim_location = pd.DataFrame()
         df_dim_location['location_id'] \
-            = df_sales_order['agreed_delivery_location']
+            = df_address['address_id']
         df_dim_location['address_line_1'] = df_address['address_line_1']
         df_dim_location['address_line_2'] = df_address['address_line_2']
         df_dim_location['district'] = df_address['district']
@@ -135,12 +152,8 @@ def lambda_handler_transform(event, context):
         df_dim_currency = pd.DataFrame()
         df_dim_currency['currency_id'] = df_currency['currency_id']
         df_dim_currency['currency_code'] = df_currency['currency_code']
-        df_dim_currency['currency_name'] = df_dim_currency['currency_name']\
-            .where(df_dim_currency['currency_code'] == 'GBP', 'pound')
-        df_dim_currency['currency_name'] = df_dim_currency['currency_name']\
-            .where(df_dim_currency['currency_code'] == 'EUR', 'euro')
-        df_dim_currency['currency_name'] = df_dim_currency['currency_name']\
-            .where(df_dim_currency['currency_code'] == 'USD', 'dollar')
+        df_dim_currency['currency_name'] = df_currency[
+            'currency_code'].apply(get_currency)
 
         final_dataframes["df_dim_currency"] = df_dim_currency
 
@@ -166,30 +179,17 @@ def lambda_handler_transform(event, context):
         df_dim_counterparty['counterparty_legal_country'] \
             = df_address['country']
         df_dim_counterparty['counterparty_legal_phone_number'] \
-            = df_address['number']
+            = df_address['phone']
 
         final_dataframes["df_dim_counterparty"] = df_dim_counterparty
 
-    upload_dataframes_to_s3(final_dataframes, transform_bucket)
+    # upload_dataframes_to_s3(final_dataframes, transform_bucket)
+    pprint(final_dataframes)
+    return 'pass'
 
-# dummy_event = {'Records': [{'eventVersion': '2.1', 'eventSource': \
-# 'aws:s3', 'awsRegion': 'eu-west-2', 'eventTime': '2025-03-07T12:18:15.393Z',\
-#  'eventName': 'ObjectCreated:Put', 'userIdentity': \
-# {'principalId': 'AWS:AROA5MSUB6C2VXKYEAC4G:Ingest-Lambda'}, \
-# 'requestParameters': {'sourceIPAddress': '18.134.5.40'}, 'responseElements':\
-#  {'x-amz-request-id': 'AEHDYJ8G0TBGYPF0', 'x-amz-id-2': \
-# 'CfelEx/b0befwraMds2+3bplAobwbQv394xeCkIEjMGrxO1+ZbD5KO2A\
-# nqL33r6fE1+ZpWvNjiF7reLxUkpelJYaQhA3HB3o'}, 's3': {'s3SchemaVersion': \
-# '1.0', 'configurationId': 'transform-trigger-notification', 'bucket': \
-# {'name': 'etl-lullymore-west-ingested', 'ownerIdentity': \
-# {'principalId': 'A3C9WEC0881B9E'}, 'arn': 'arn:aws:s3:::\
-# etl-lullymore-west-ingested'}, 'object': {'key': \
-# '2025/3++++++++++++++++/totesys-data-ingested-2025-03-07+\
-# 16%3A57%3A20.144585', 'size': 320, 'eTag': \
-# '0f067386df0e327887119e95427e7327', 'sequencer': '0067CAE407595EA004'}}}]}
 
-# if __name__ == '__main__':
-#     lambda_handler_transform(dummy_event, 'context')
+if __name__ == '__main__':
+    lambda_handler_transform('event', 'context')
 
 
 def upload_dataframes_to_s3(dataframes_dict, transform_bucket):
