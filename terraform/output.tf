e0b711677b02dddf45622ce784d0fb886a701ff2
Lucy Milligan <lucyjmilligan@yahoo.co.uk>
2025-03-12 11:17:04 +0000

    update files for deployment -update TF

--- terraform/output.tf
+++ terraform/output.tf
@@ -3,7 +3,7 @@ output "aws_lambda_function_arn" {
   value         = aws_lambda_function.ingest_function.arn
 }
 
-output "aws_state_machine" {
-    description = "The ARN for the State Machine"
-    value       = aws_sfn_state_machine.sfn_state_machine.name
-}
+# output "aws_state_machine" {
+#     description = "The ARN for the State Machine"
+#     value       = aws_sfn_state_machine.sfn_state_machine.name
+# }
